0818a2bbfee6f2aa9c4c2266faa6c5e710913a20
aravinthankumar <aravinthankumara@gmail.com>
2022-03-10 21:21:22 +0530

    New Code

--- src/test/java/org/git/GGiitt.java
+++ src/test/java/org/git/GGiitt.java
@@ -8,43 +8,25 @@ public class GGiitt extends BaseClass {
 
 	public static void main(String[] args) throws InterruptedException {
 		chromeDriver();
-		
 		urlLounch("https://www.flipkart.com/");
 		implicitwait();
-		
 		GitSub z = new GitSub();
-		
 		click(z.getClkCancel());
-		
-	     sendkeysValue(z.getTxtProductName(), "apple mobile", Keys.ENTER);
-	     
+	     sendkeysValue(z.getTxtProductName(), "apple mobile", Keys.ENTER); 
 		driver.navigate().refresh();
-		
 		System.out.println("...........Low price..................");
 		click(z.getLowPrice());
-		
 			for (int i = 1; i <=23; i++) {
 			findElemenys((char) i, "(//div[@class='_4rR01T'])");
 		findElemenys((char)i, "(//div[@class='_30jeq3 _1_WHN1'])");
-	
 			}
 			Thread.sleep(4000);
 			System.out.println("high price..............");
-			
 			click(z.getHighPrice());
-			
 			for (int i =1 ; i <=20; i++) {
 				findElemenys((char) i, "(//div[@class='_4rR01T'])");
 			findElemenys((char)i, "(//div[@class='_30jeq3 _1_WHN1'])");
 				}
-			
 			driver.quit();
-			
-			
-			
-	
-	
-		
 	}
-
 }
